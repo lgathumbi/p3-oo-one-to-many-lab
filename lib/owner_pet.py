9e4a65e2db996086c8fa98db5c460088794c8fb1
lorna gathumbi <nyawiralornah@gmail.com>
2024-12-08 11:53:50 +0300

    one to many

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,34 @@
 class Pet:
     pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    def __init__(self,name,pet_type,owner=None):
+        self.name = name
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception
+        Pet.all.append(self)
+        self.pet_type = pet_type
+        self.owner = owner
 
 class Owner:
     pass
+    def __init__(self, name):
+        self.name = name
+    def pets(self):
+        return [ pet for pet in Pet.all if pet.owner == self ]
+    
+    def add_pet(self, new_pet):
+        if isinstance(new_pet, Pet):
+            new_pet.owner = self
+        else:
+            raise Exception("NO")
+    
+    def get_sorted_pets(self):
+        # GET MY PETS
+        my_pets = self.pets()
+        # SORT MY PETS
+        sorted_pets = sorted( my_pets, key = lambda each_pet: each_pet.name.lower() )
+        # RETURN EM
+        return sorted_pets
